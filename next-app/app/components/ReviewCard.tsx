a0ce1245f6fe676669bc0b4cfcbc62143e2c0580
Yu Lai <yulai.linda@gmail.com>
2024-11-04 11:52:10 -0500

    display categories in Marquee

--- next-app/app/components/ReviewCard.tsx
+++ next-app/app/components/ReviewCard.tsx
@@ -1,59 +1,11 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import Marquee from "@/components/ui/marquee";
+import { Category, Tag } from "@prisma/client";
+import { useEffect, useState } from "react";
 
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-];
-
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
-
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
+const ReviewCard = ({ name }: { name: string }) => {
   return (
     <figure
       className={cn(
@@ -65,30 +17,48 @@ const ReviewCard = ({
       )}
     >
       <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        {/* <img className="rounded-full" width="32" height="32" alt="" src={img} /> */}
         <div className="flex flex-col">
           <figcaption className="text-sm font-medium dark:text-white">
             {name}
           </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
+          {/* <p className="text-xs font-medium dark:text-white/40">{username}</p> */}
         </div>
       </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
+      {/* <blockquote className="mt-2 text-sm">{body}</blockquote> */}
     </figure>
   );
 };
 
 export default function MarqueeDemo() {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const categoryResponse = await fetch(
+        "http://localhost:3000/api/categories",
+        {
+          next: { revalidate: 10 },
+        }
+      );
+      const categoryData = await categoryResponse.json();
+      setCategories(categoryData);
+    };
+    fetchCategories();
+  }, []);
+
+  const firstRow = categories.slice(0, categories.length / 2);
+  const secondRow = categories.slice(categories.length / 2);
   return (
     <div className="relative flex h-[200px] w-full flex-col items-center justify-center rounded-lg">
-      <Marquee pauseOnHover className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+      <Marquee pauseOnHover className="[--duration:35s]">
+        {firstRow.map((category) => (
+          <ReviewCard key={category.name} {...category} />
         ))}
       </Marquee>
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+      <Marquee reverse pauseOnHover className="[--duration:35s]">
+        {secondRow.map((category) => (
+          <ReviewCard key={category.name} {...category} />
         ))}
       </Marquee>
       {/* <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
