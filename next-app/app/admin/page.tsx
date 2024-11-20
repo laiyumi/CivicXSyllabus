4d1e56dfbfc09865a4c77039c6d9dc0db2628814
Yu Lai <yulai.linda@gmail.com>
2024-11-20 11:54:28 -0500

    removed useSession in admin page

--- next-app/app/admin/page.tsx
+++ next-app/app/admin/page.tsx
@@ -4,19 +4,11 @@ import DashboardSummary from "./DashboardSummary";
 import { useSession } from "next-auth/react";
 
 const AdminHomePage = () => {
-  // session is always non-null inside this component
-  const { data: session } = useSession();
-
   return (
     <>
       <DashboardSummary />
     </>
   );
-};
-
-AdminHomePage.auth = {
-  role: "ADMIN",
-  unauthorized: "/",
 };
 
 export default AdminHomePage;
