308da988fb774e5e7606534cb67e61e0aa00988b
Yu Lai <yulai.linda@gmail.com>
2025-01-26 15:45:19 -0500

    revised footer layout

--- next-app/app/Footer.tsx
+++ next-app/app/Footer.tsx
@@ -4,36 +4,43 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer bg-neutral text-neutral-content p-10">
-      <aside>
-        <img src="/new-logo-white.png" className="w-24 pb-2" alt="Site logo" />
-        <p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
-      </aside>
-      <nav>
-        <h3 className="footer-title">Social</h3>
-        <div className="grid grid-flow-col gap-4">
-          <Link href="https://www.linkedin.com/company/civicxsyllabus">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <title id="linkedin-title">LinkedIn logo</title>
-              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-            </svg>
-          </Link>
-        </div>
-      </nav>
-      <nav>
-        <h3 className="footer-title">Legal</h3>
+      <div>
+        <Link href="/" className="w-24">
+          <img
+            src="/new-logo-white.png"
+            className="w-24 pb-2"
+            alt="Site logo"
+          />
+        </Link>
+        <p>Copyright © {new Date().getFullYear()} - All rights reserved.</p>
+      </div>
+      <div className="flex gap-4 justify-self-end self-end items-center">
         <Link href="/privacy" className="link link-hover">
           Privacy Policy
         </Link>
-        <Link href="/contact" className="link link-hover">
+        <span>|</span>
+        <Link
+          href={`mailto:civicxsyllabus@gmail.com?`}
+          target="_blank"
+          className="link link-hover"
+          rel="noopener noreferrer"
+        >
           Contact Us
         </Link>
-      </nav>
+        <span>|</span>
+        <Link href="https://www.linkedin.com/company/civicxsyllabus">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            className="fill-current"
+          >
+            <title id="linkedin-title">LinkedIn logo</title>
+            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+          </svg>
+        </Link>
+      </div>
     </footer>
   );
 };
