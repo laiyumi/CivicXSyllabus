a57edade90b0c5cab096a62d1d992a2e84b2feda
Yu Lai <yulai.linda@gmail.com>
2025-02-06 11:37:18 -0500

    revised sign in form

--- next-app/app/auth/signin/page.tsx
+++ next-app/app/auth/signin/page.tsx
@@ -4,10 +4,29 @@ import { useState, useEffect } from "react";
 import { getCsrfToken, signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { SignInSchema } from "../../validationSchemas";
+import Spinner from "../../components/Spinner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import ErrorMessage from "../../components/ErrorMessage";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-export default function SignIn() {
+type SignInFormInputs = z.infer<typeof SignInSchema>;
+
+const SignInPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormInputs>({
+    resolver: zodResolver(SignInSchema),
+  });
+
   const router = useRouter();
   const [csrfToken, setCsrfToken] = useState<string | null>(null);
+
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     async function fetchCsrfToken() {
@@ -21,22 +40,30 @@ export default function SignIn() {
     fetchCsrfToken();
   }, []);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true);
+      const res = await signIn("credentials", {
+        email: data.email,
+        password: data.password,
+        redirect: false,
+      });
 
-    const res = await signIn("credentials", {
-      email: formData.get("email"),
-      password: formData.get("password"),
-      redirect: false,
-    });
+      if (res?.error) {
+        if (res.error === "CredentialsSignin") {
+          setError("Invalid email or password");
+        }
+        console.log(error);
+        setIsSubmitting(false);
+        return;
+      }
 
-    if (res?.error) {
-      alert("Invalid email or password");
-    } else {
       router.push("/");
+    } catch (error) {
+      setIsSubmitting(false);
+      setError("An unexpected error occurred on validation");
     }
-  };
+  });
 
   return (
     <div className="grid grid-cols-12">
@@ -45,32 +72,54 @@ export default function SignIn() {
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl text-center font-normal">Sign In</h1>
           </div>
+          {/* display input error */}
+          {error && (
+            <div role="alert" className="alert alert-error mb-4 w-1/2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <title>Error alert icon</title>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>{error}</span>
+            </div>
+          )}
+          {/* Sign in form */}
           <div className="flex flex-col gap-6">
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col space-y-3 w-72"
-            >
+            <form onSubmit={onSubmit} className="flex flex-col gap-4 w-72">
               {/* CSRF Token (Hidden Input) */}
               <input name="csrfToken" type="hidden" value={csrfToken || ""} />
-              <label className="flex flex-col">
-                Email
+              <label className="form-control w-full flex gap-2">
+                <span className="text-m">Email</span>
                 <input
-                  name="email"
-                  type="email"
-                  className="border p-2 rounded"
-                  required
+                  type="text"
+                  className="input input-bordered w-full"
+                  {...register("email")}
                 />
+                <ErrorMessage>{errors.email?.message}</ErrorMessage>
               </label>
-              <label className="flex flex-col">
-                Password
+              <label className="form-control w-full flex gap-2">
+                <span className="text-m">Password</span>
                 <input
-                  name="password"
                   type="password"
-                  className="border p-2 rounded"
-                  required
+                  className="input input-bordered w-full"
+                  {...register("password")}
                 />
+                <ErrorMessage>{errors.password?.message}</ErrorMessage>
               </label>
-              <button type="submit" className="btn btn-primary">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="btn btn-primary mt-4"
+              >
                 Sign In
               </button>
             </form>
@@ -91,34 +140,36 @@ export default function SignIn() {
                 onClick={() => signIn("google", { callbackUrl: "/" })}
                 className="btn btn-secondary w-full "
               >
-                <div className="">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    width="40"
-                    height="40"
-                    viewBox="0 0 48 48"
-                  >
-                    <path
-                      fill="#FFC107"
-                      d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                    ></path>
-                    <path
-                      fill="#FF3D00"
-                      d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                    ></path>
-                    <path
-                      fill="#4CAF50"
-                      d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                    ></path>
-                    <path
-                      fill="#1976D2"
-                      d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                    ></path>
-                  </svg>{" "}
+                <div className="flex items-center justify-center gap-2">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="30"
+                      height="30"
+                      viewBox="0 0 48 48"
+                    >
+                      <path
+                        fill="#FFC107"
+                        d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                      ></path>
+                      <path
+                        fill="#FF3D00"
+                        d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                      ></path>
+                      <path
+                        fill="#4CAF50"
+                        d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                      ></path>
+                      <path
+                        fill="#1976D2"
+                        d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                      ></path>
+                    </svg>{" "}
+                  </div>
+                  Sign in with Google
                 </div>
-                Sign in with Google
               </button>
             </div>
           </div>
@@ -126,4 +177,6 @@ export default function SignIn() {
       </div>
     </div>
   );
-}
+};
+
+export default SignInPage;
--- next-app/app/validationSchemas.ts
+++ next-app/app/validationSchemas.ts
@@ -30,6 +30,11 @@ export const UserSubmittedResourceSchema = z.object({
   categories: z.string().optional(),
 });
 
+export const SignInSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
 export const CreateAccountSchema = z
   .object({
     email: z.string().email("Please enter a valid email address"),
