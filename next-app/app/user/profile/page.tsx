8dfe8f142cd8396d51315b5291dd5d747e003a38
Yu Lai <yulai.linda@gmail.com>
2025-03-13 12:49:55 -0400

    Created '/api/change-password' endpoint and implemented client side to change password

--- next-app/app/user/profile/page.tsx
+++ next-app/app/user/profile/page.tsx
@@ -5,9 +5,29 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import FadeOutMessage from "@/app/components/FadeOutMessage";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ResetPasswordSchema } from "@/app/validationSchemas";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import { z } from "zod";
+import { set } from "date-fns";
+
+type ResetPasswordFormInputs = z.infer<typeof ResetPasswordSchema>;
 
 const UserProfilePage = () => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<ResetPasswordFormInputs>({
+    resolver: zodResolver(ResetPasswordSchema),
+    mode: "onChange",
+  });
+
   const { data: session, status, update } = useSession();
+
   const router = useRouter();
 
   const userId = session?.user?.id || "";
@@ -15,14 +35,16 @@ const UserProfilePage = () => {
   const [userEmail, setUserEmail] = useState("");
   const [userImage, setUserImage] = useState("");
 
+  const [password, setPassword] = useState("********");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+
   const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
-  const [message, setMessage] = useState("");
 
   const [isNameEditing, setIsNameEditing] = useState(false);
   const [isPasswordEditing, setIsPasswordEditing] = useState(false);
+
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
 
   const [key, setKey] = useState(0);
 
@@ -77,22 +99,36 @@ const UserProfilePage = () => {
     }
   };
 
-  const handlePasswordChange = async () => {
-    if (password !== confirmPassword) {
-      setError("Passwords do not match.");
-      return;
-    }
+  const onClickChangePassword = () => {
+    setIsPasswordEditing(true);
+    setPassword("");
+  };
+
+  const handlePasswordChange = handleSubmit(async (data) => {
+    const requestData = {
+      email: userEmail,
+      // resetToken: resetCode,
+      newPassword: data.newPassword,
+      confirmedNewPassword: data.confirmedNewPassword,
+    };
+
+    console.log(requestData);
 
     try {
-      const response = await axios.put("/api/users/update-password", {
-        password,
-      });
-      setMessage("Password updated successfully!");
+      const { data } = await axios.post("/api/change-password", requestData);
+
+      setMessage("Password has been reset successfully!");
+      setError("");
+
+      setPassword("");
+      setConfirmedPassword("");
       setIsPasswordEditing(false);
     } catch (error: any) {
+      console.log(error);
       setError(error.response?.data?.error || "An unexpected error occurred.");
+      setMessage("");
     }
-  };
+  });
 
   return (
     <>
@@ -107,10 +143,10 @@ const UserProfilePage = () => {
           Email
           <span className="text-gray-600">{userEmail}</span>
         </div>
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 ">
           <div className="flex gap-4 relative items-center">
-            <label className="input input-bordered flex items-center gap-4 w-64">
-              Name
+            <label className="input input-bordered flex items-center gap-4 w-72 grow">
+              <span>Name</span>
               {isNameEditing ? (
                 <input
                   type="text"
@@ -125,7 +161,10 @@ const UserProfilePage = () => {
             <div className="absolute left-full ml-4">
               {isNameEditing ? (
                 <div className="flex gap-2">
-                  <button className="btn btn-md" onClick={handleNameChange}>
+                  <button
+                    className="btn btn-md btn-primary"
+                    onClick={handleNameChange}
+                  >
                     Save
                   </button>
                   <button
@@ -137,7 +176,7 @@ const UserProfilePage = () => {
                 </div>
               ) : (
                 <button
-                  className="btn btn-md"
+                  className="btn btn-md btn-primary"
                   onClick={() => setIsNameEditing(true)}
                 >
                   Change
@@ -145,65 +184,95 @@ const UserProfilePage = () => {
               )}
             </div>
           </div>
-          <div className="flex flex-col gap-4 ">
-            <div className="flex gap-4 relative items-center">
-              <label className="input input-bordered flex items-center gap-4 w-64">
-                Password
-                {isPasswordEditing ? (
-                  <>
+
+          {/* Edit password */}
+          <div className="flex flex-col gap-8">
+            <form
+              onSubmit={handlePasswordChange}
+              className="flex flex-col gap-8"
+            >
+              <div className="relative flex gap-4 items-center">
+                <div className="w-72">
+                  <label className="input input-bordered flex items-center gap-4  first-line:grow">
+                    <span className="whitespace-nowrap">
+                      {isPasswordEditing ? "New Password" : "Password"}
+                    </span>
+
+                    <div className="flex flex-col gap-2">
+                      <input
+                        type="password"
+                        className="text-gray-600 flex-grow"
+                        disabled={!isPasswordEditing}
+                        placeholder={isPasswordEditing ? "" : "••••••••"}
+                        {...register("newPassword")}
+                        onFocus={(e) => {
+                          if (!isPasswordEditing) {
+                            e.target.blur(); // Prevents editing unless 'Change' is clicked
+                          }
+                        }}
+                      />
+                    </div>
+                  </label>
+                  {isPasswordEditing && errors.newPassword && (
+                    <ErrorMessage>{errors.newPassword.message}</ErrorMessage>
+                  )}
+                </div>
+
+                <div className="absolute left-full ml-4">
+                  {isPasswordEditing ? (
+                    <div className="flex gap-2">
+                      <button className="btn btn-md btn-primary" type="submit">
+                        Save
+                      </button>
+                      <button
+                        className="btn btn-md"
+                        onClick={() => {
+                          setIsPasswordEditing(false);
+                          setValue("newPassword", "");
+                          setValue("confirmedNewPassword", "");
+                        }}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  ) : (
+                    <button
+                      className="btn btn-md btn-primary"
+                      onClick={() => {
+                        setIsPasswordEditing(true);
+                        setValue("newPassword", "");
+                      }}
+                    >
+                      Change
+                    </button>
+                  )}
+                </div>
+              </div>
+
+              {isPasswordEditing && (
+                <div className="flex flex-col gap-4 relative items-center">
+                  <label className="input input-bordered flex items-center gap-4 w-72 grow">
+                    <span className="whitespace-nowrap">
+                      Confirmed Password
+                    </span>
                     <input
                       type="password"
-                      className="text-gray-600"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
+                      className="text-gray-600 flex-grow"
+                      // value={confirmedPassword}
+                      {...register("confirmedNewPassword")}
                     />
-                  </>
-                ) : (
-                  <>
-                    <span>********</span>
-                  </>
-                )}
-              </label>
-            </div>
-
-            <button
-              className="btn btn-md left-full ml-4"
-              onClick={() => setIsPasswordEditing(true)}
-            >
-              Change
-            </button>
-
-            <div className=" left-full ml-4">
-              <div className="flex gap-2 ">
-                <button className="btn btn-md" onClick={handlePasswordChange}>
-                  Save
-                </button>
-                <button
-                  className="btn btn-md"
-                  onClick={() => setIsPasswordEditing(false)}
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-
-            {/* 
-            {isPasswordEditing && (
-              <div>
-                <label className="input input-bordered flex items-center gap-4 w-64">
-                  Confirmed Password
-                  <input
-                    type="password"
-                    className="text-gray-600"
-                    placeholder="Confirm password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                  />
-                </label>
-              </div>
-            )} */}
+                  </label>
+                  {isPasswordEditing && errors.confirmedNewPassword && (
+                    <ErrorMessage>
+                      {errors.confirmedNewPassword?.message}
+                    </ErrorMessage>
+                  )}
+                </div>
+              )}
+            </form>
           </div>
-
+        </div>
+        <div>
           {error && (
             <FadeOutMessage
               key={key}
