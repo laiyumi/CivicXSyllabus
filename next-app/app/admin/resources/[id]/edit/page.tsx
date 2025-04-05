6af4b736be42dda3504b08dec5aeb7dc11fa24de
Yu Lai <yulai.linda@gmail.com>
2025-04-04 20:17:57 -0400

    Shows the success message after updating a resource on admin dashboard

--- next-app/app/admin/resources/[id]/edit/page.tsx
+++ next-app/app/admin/resources/[id]/edit/page.tsx
@@ -1,51 +1,51 @@
-import React, { useEffect } from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import ResourceForm from "../../components/ResourceForm";
 import prisma from "@/prisma/client";
+import { Prisma } from "@prisma/client";
+import type { Post } from "@prisma/client";
 import { notFound } from "next/navigation";
 import AdminDashboardNavBar from "@/app/admin/NavBar";
 import EditResourceForm from "../../components/EditResourceForm";
 import axios from "axios";
+import { useNotifications } from "../../../../contexts/NotificationContext";
 
 interface Props {
   params: { id: string };
 }
 
-const EditResourcePage = async ({ params }: Props) => {
-  const resource = await prisma.post.findUnique({
-    where: { id: params.id },
-    include: {
-      categories: {
-        select: {
-          id: true,
-          name: true,
-        },
-      },
-      tags: {
-        select: {
-          id: true,
-          name: true,
-        },
-      },
-      source: {
-        select: {
-          id: true,
-          name: true,
-        },
-      },
-    },
-  });
+type PostWithRelations = Prisma.PostGetPayload<{
+  include: { categories: true; tags: true; source: true };
+}>;
 
-  if (!resource) {
-    notFound();
-  }
+const EditResourcePage = ({ params }: Props) => {
+  const { showNotification, clearAllNotifications } = useNotifications();
+  const [resource, setResource] = useState<PostWithRelations>();
 
-  // console.log("-----: " + resource.categories.map((category) => category.id));
-  console.log(resource);
+  useEffect(() => {
+    const fetchResource = async () => {
+      try {
+        const response = await axios.get(`/api/resources/${params.id}`);
+        const resourceData = response.data;
+        if (!resourceData) {
+          notFound();
+        }
+
+        setResource(resourceData);
+        console.log("Resource data: ", resourceData);
+      } catch (error) {
+        console.error("Error fetching resource:", error);
+      }
+    };
+
+    fetchResource();
+  }, [params.id]);
 
   return (
     <div className="flex">
       <div className="w-full">
-        <EditResourceForm resource={resource} />
+        {resource && <EditResourceForm resource={resource} />}
       </div>
     </div>
   );
