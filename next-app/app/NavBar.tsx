0192b1ef694da445c30079804786321b450e7acc
Yu Lai <yulai.linda@gmail.com>
2025-06-28 18:33:44 -0400

    Enhance UX:Add arrow indicator to the nav bar dropdown

--- next-app/app/NavBar.tsx
+++ next-app/app/NavBar.tsx
@@ -7,6 +7,25 @@ import ThemeController from "./components/ThemeController";
 import ThemeLogo from "./components/ThemeLogo";
 
 const NavBar = () => {
+  const [aboutDropdownOpen, setAboutDropdownOpen] = useState(false);
+  const [howToUseDropdownOpen, setHowToUseDropdownOpen] = useState(false);
+
+  const handleAboutDropdownToggle = () => {
+    setAboutDropdownOpen(!aboutDropdownOpen);
+  };
+
+  const handleHowToUseDropdownToggle = () => {
+    setHowToUseDropdownOpen(!howToUseDropdownOpen);
+  };
+
+  const handleHowToUseDropdownClose = () => {
+    setHowToUseDropdownOpen(false);
+  };
+
+  const handleAboutDropdownClose = () => {
+    setAboutDropdownOpen(false);
+  };
+
   return (
     <>
       <div className="navbar p-5 text-base-content bg-base-300 relative z-10">
@@ -102,34 +121,69 @@ const NavBar = () => {
           >
             Resources
           </Link>
-          <div className="dropdown dropdown-hover">
+          <div className="dropdown">
             <div
               data-test="how-to-use-link-large-screen"
               tabIndex={0}
               role="button"
-              className="btn btn-ghost m-1"
+              className="btn btn-ghost m-1 flex items-center gap-1"
+              onClick={handleHowToUseDropdownToggle}
             >
               How to Use
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={`w-4 h-4 transition-transform duration-200 ${
+                  howToUseDropdownOpen ? "rotate-180" : ""
+                }`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
             </div>
             <ul
               tabIndex={0}
-              className="text-base-content dropdown-content menu bg-base-300 rounded-box z-20 w-52 p-2 shadow"
+              className={`text-base-content dropdown-content menu bg-base-100 rounded-box z-20 w-52 p-2 shadow ${
+                howToUseDropdownOpen ? "block" : "hidden"
+              }`}
             >
               <li>
-                <Link href="/build-my-syllabus">Use Cases</Link>
+                <Link
+                  href="/build-my-syllabus"
+                  onClick={handleHowToUseDropdownClose}
+                >
+                  Use Cases
+                </Link>
               </li>
               <li>
-                <Link href="/build-my-syllabus/before-you-begin">
+                <Link
+                  href="/build-my-syllabus/before-you-begin"
+                  onClick={handleHowToUseDropdownClose}
+                >
                   Civic 101
                 </Link>
               </li>
               <li>
-                <Link href="/build-my-syllabus/build-partnerships">
+                <Link
+                  href="/build-my-syllabus/build-partnerships"
+                  onClick={handleHowToUseDropdownClose}
+                >
                   Build Partnerships
                 </Link>
               </li>
               <li>
-                <Link href="/build-my-syllabus/get-to-work">Get to Work</Link>
+                <Link
+                  href="/build-my-syllabus/get-to-work"
+                  onClick={handleHowToUseDropdownClose}
+                >
+                  Get to Work
+                </Link>
               </li>
             </ul>
           </div>
@@ -138,22 +192,50 @@ const NavBar = () => {
               data-test="about-link-large-screen"
               tabIndex={0}
               role="button"
-              className="btn btn-ghost m-1"
+              className="btn btn-ghost m-1 flex items-center gap-1"
+              onClick={handleAboutDropdownToggle}
             >
               About
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={`w-4 h-4 transition-transform duration-200 ${
+                  aboutDropdownOpen ? "rotate-180" : ""
+                }`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
             </div>
             <ul
               tabIndex={0}
-              className="text-base-content dropdown-content menu bg-base-300 rounded-box z-[1] w-52 p-2 shadow"
+              className={`text-base-content dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow ${
+                aboutDropdownOpen ? "block" : "hidden"
+              }`}
             >
               <li>
-                <Link href="/about">About Us</Link>
+                <Link href="/about" onClick={handleAboutDropdownClose}>
+                  About Us
+                </Link>
               </li>
               <li>
-                <Link href="/about/team">Our Team</Link>
+                <Link href="/about/team" onClick={handleAboutDropdownClose}>
+                  Our Team
+                </Link>
               </li>
               <li>
-                <Link href="/about/who-we-serve">Who We Serve</Link>
+                <Link
+                  href="/about/who-we-serve"
+                  onClick={handleAboutDropdownClose}
+                >
+                  Who We Serve
+                </Link>
               </li>
             </ul>
           </div>
