e6819a775dcabe4ca1fbc88d1d174b885e8f5135
Yu Lai <yulai.linda@gmail.com>
2025-07-24 10:12:27 -0400

    added Aruna profile

--- next-app/app/about/team/page.tsx
+++ next-app/app/about/team/page.tsx
@@ -132,6 +132,44 @@ const TeamPage = () => {
             </p>
           </div>
         </div>
+
+        <div className="md:grid md:grid-cols-3 xs:flex xs:flex-col bg-base-100 mb-8 rounded-2xl">
+          <div className="col-span-2 p-8">
+            <div className="flex justify-between pb-6 text-base-content">
+              <div>
+                <h2 className="card-title">Aruna Senthamaraikannan</h2>
+                <p className="text-md">Civic Research Assistant</p>
+              </div>
+              <Link href="https://www.linkedin.com/in/aruna-senthamaraikannan-8b1946210/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="white"
+                  className="bi bi-linkedin text-base-content fill-current hover:text-blue-700"
+                  viewBox="0 0 16 16"
+                >
+                  <title>Aruna&apos;s LinkedIn</title>
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                </svg>
+              </Link>
+            </div>
+            <p className="text-base-content">
+              Aruna is a Software Engineering Systems graduate student at
+              Northeastern University with hands-on experience in software
+              development. She’s passionate about crafting user-focused
+              solutions that make an impact. Outside tech, she’s a culinary
+              explorer perfecting the art of South Indian cuisine.
+            </p>
+          </div>
+          <figure className="col-span-1">
+            <img
+              src="/aruna3.jpg"
+              alt="The profile image of Aruna Senthamaraikannan"
+              className="object-cover w-full h-full rounded-2xl"
+            />
+          </figure>
+        </div>
       </div>
     </>
   );
